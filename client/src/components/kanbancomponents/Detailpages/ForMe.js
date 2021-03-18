a5e52b526c93e31a7f2ccdb7c6a12844c88ad297
Lea Weissman <lea.weissman@icloud.com>
2021-03-18 12:39:45 +0100

    setup of the info page for deeper insights on Kanban

---
+++ client/src/components/kanbancomponents/Detailpages/ForMe.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export default function ForMe() {
+    return (
+        <InfoCardInStyle>
+            <InfoHeaderInStyle>What can it do for me?</InfoHeaderInStyle>
+            <InfoTextInStyle>Bavaria ipsum dolor sit amet Breihaus i waar soweid, a Maß und no a Maß! I moan oiwei Fünferl Almrausch Biagadn wuid? Fias schoo sog i Musi Deandlgwand fias Guglhupf, es heitzdog so. Glacht af da gwiss, Hetschapfah Gamsbart in da greana Au. Hi Bladl Blosmusi glacht des. Barfuaßat Greichats midanand mogsd a Bussal Watschnbaam Deandlgwand i hob di liab oans Kneedl! Wurscht Radler Zidern a geh Mongdratzal Heimatland. Biazelt san nia need hinter’m Berg san a no Leit, Goaßmaß mechad i mechad dee Schwoanshaxn spernzaln. Goaßmaß Meidromml weida vui huift vui, Haferl. Is ma Wuascht griaß God beinand measi Gschicht etza Weißwiaschd, oba sowos Zwedschgndadschi oans pfundig. Scheans hogg di hera am acht’n Tag schuf Gott des Bia hob i an Suri auffi Broadwurschtbudn mechad auf’d Schellnsau, wolpern mehra!
+
+            Oamoi Breihaus des wiad a Mordsgaudi guad, ded Schorsch dahoam i Biakriagal Marei. Is des is schee Kaiwe Engelgwand Servas Mongdratzal. Woaß Guglhupf Obazda mim a bissal wos gehd ollaweil, an. Charivari Breihaus ja, wo samma denn, auffi i hab an hogg ma uns zamm Mongdratzal ozapfa a bravs wuid da. Mi ghupft wia gsprunga Maibam Schdeckalfisch wo hi Radler. Auffi Edlweiss gelbe Rüam Enzian, wuid. Gwihss san nimma zünftig hallelujah sog i, luja barfuaßat i hob di liab owe Spezi, Schneid Leonhardifahrt. Guglhupf auszutzeln Leonhardifahrt Schorsch um Godds wujn so i daad mehra i moan scho aa. Kimmt Engelgwand heid zünftig, mechad Freibia. Sepp Wiesn Ledahosn obacht auf’d Schellnsau zünftig Brezn da Kini Weibaleid, hoggd.
+
+Semmlkneedl luja auf gehds beim Schichtl, Graudwiggal! Hoid mim Radl foahn Sauwedda am acht’n Tag schuf Gott des Bia, Enzian oamoi wuid kloan. Marei bitt is, Obazda o’ha dahoam Ewig und drei Dog Sepp Sauwedda! Maßkruag Steckerleis liberalitas Bavariae Mongdratzal, ned blärrd Obazda am acht’n Tag schuf Gott des Bia Watschnbaam? Graudwiggal Weibaleid greaßt eich nachad glacht Foidweg Habedehre kimmt in da greana Au an nia need vo de! Sog i Weiznglasl Heimatland a bravs ded. Heimatland Sauakraud nomoi i moan scho aa, Biakriagal des basd scho abfieseln wolpern. Heitzdog wo hi gscheit Klampfn! Radi gfreit mi helfgod Ewig und drei Dog Habedehre, Marterl mechad spernzaln schoo. Pfenningguat hoggd greaßt eich nachad damischa jo mei is des schee back mas auf gehds beim Schichtl Wurscht Oachkatzlschwoaf ja, wo samma denn? Heitzdog ghupft wia gsprunga di. </InfoTextInStyle>
+        </InfoCardInStyle>
+    );
+}
+
+const InfoCardInStyle = styled.div`
+background-color: white;
+border-radius: 5rem; 
+border: 2px solid var(--primarypink);
+margin: 2rem;
+`
+const InfoHeaderInStyle = styled.h3`
+text-align:center;
+margin: 1rem;
+`
+const InfoTextInStyle = styled.p`
+margin: 2rem;
+`
