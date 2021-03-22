7837dce1654861b48421eaaa7e439d946da2e89c
Lea Weissman <lea.weissman@icloud.com>
2021-03-22 12:08:34 +0100

    startimg of with OKR

--- client/src/pages/okr.js
+++ client/src/pages/okr.js
@@ -1,8 +1,22 @@
+import styled from 'styled-components';
 
 export default function Okr() {
     return (
         <div>
             <p>Welcome to OKR</p>
+            <Infotag>Objectives and Key Results (OKR) ist eine beliebte Technik, um Ziele innerhalb einer Organisation zu setzen. Die Hauptanwendung besteht darin, Unternehmens-, Team- und persönliche Ziele zu verbinden, um messbare Ergebnisse zu erhalten. Die OKR-Methode wird von Unternehmen wie Google, Linkedin, Twitter usw. verwendet, um ihr Team bei der Erreichung ihrer Ziele zu unterstützen.</Infotag>
         </div>
     );
 }
+
+
+
+
+const Infotag = styled.p`
+text-align: center;
+border: solid 2px var(--primarypink);
+border-radius: 1rem;
+margin: 1rem; 
+padding: 0.8rem;
+background-color: white;
+`
